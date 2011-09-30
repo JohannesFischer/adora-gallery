c1d792820384f61c3d77b77fe54fbaf0518f3663
JohannesFischer <git@johannes-fischer.de>
2011-09-30 23:17:26 +0200

    fill filedatetime with the correct exif tag

--- application/views/web/includes/admin_update_form.php
+++ application/views/web/includes/admin_update_form.php
@@ -16,10 +16,10 @@
 		<label>Description</label>
 		<textarea name="Description" placeholder="Description" cols="45" rows="5"></textarea>
 		<input type="hidden" name="Filename" value="<?=$source_file;?>" />
-		<?php if($exif['FileDateTime']): ?>
-		<input type="hidden" name="FileDateTime" value="<?=$exif['FileDateTime'];?>" />
-		<input type="hidden" name="Orientation" value="<?=$exif['Orientation'];?>" />
+		<?php if($exif['DateTimeOrigina']): ?>
+		<input type="hidden" name="FileDateTime" value="<?=$exif['DateTimeOrigina'];?>" />
 		<?php endif; ?>
+		<input type="hidden" name="Orientation" value="<?=$exif['Orientation'];?>" />
 		<input type="submit" name="save_image" value="save" />
 	</fieldset>
 </form>
